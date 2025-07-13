7a49ec135c394d5e75d307dcefff7ef24342c3c9
Minigamer42 <51682780+Minigamer42@users.noreply.github.com>
2025-07-13 23:29:43 +0200

    Girls Band Cry pr sorter

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "bandori",
-    title: "Bang Dream! Sorter",
+    localStoragePrefix: "girls-band-cry",
+    title: "Girls Band Cry Sorter",
     description: "Party rank sorter for your custom list of songs."
 };
